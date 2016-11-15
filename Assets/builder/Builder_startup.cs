701103c411f63a382c566525bc0aa6e9b39bb030
ilushareg <ilushareg@gmail.com>
2016-11-15 09:12:15 +0300

    initial Import

---
+++ Assets/builder/Builder_startup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Builder_startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //create terrain
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
